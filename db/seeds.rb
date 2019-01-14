f4bbe16825ae5c1b6fbbd1e7505fa7d79232f8fc
charith08 <charithsaivadla@gmail.com>
2019-01-14 16:48:22 +0530

    invalid link2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name:  "Charith",
-             email: "charithsaivadla@commutatus.com",
+             email: "charithsaivadla@gmail.com",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true,
