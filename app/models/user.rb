0613dd61debcdcdf023e5931d9adb2000f978302
charith08 <charithsaivadla@gmail.com>
2019-01-17 17:55:20 +0530

    passwordlink

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ApplicationRecord
 attr_accessor :remember_token, :activation_token, :reset_token
 before_save   :downcase_email
 before_create :create_activation_digest
+APPROVED_DOMAINS = ["commutatus.com"]
 
+def domain_check
+  unless APPROVED_DOMAINS.any? { |word| email.end_with?(word)}
+    errors.add(:email, "only commutatus domain")
+  end
+end
 
-validates :name, presence: true, length:  {maximum: 50 }
+validates :name, presence: true,if: :domain_check, length:  {maximum: 50 }
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 validates :email, presence: true, length: { maximum: 255 },
                 format: { with: VALID_EMAIL_REGEX },
